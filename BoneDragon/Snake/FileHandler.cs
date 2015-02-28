146759fe243b4b596dcd20f80557528b3b339aba
r1f <ivaylo.arnaudov12@gmail.com>
2015-02-28 22:12:38 +0200

    Now using LINQ to XML instead of XmlDocument
    Added score sorting functionality. Now using "LINQ to XML" instead of
    XmlDocument for better performance & code readability.

--- BoneDragon/Snake/FileHandler.cs
+++ BoneDragon/Snake/FileHandler.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Xml;
 using System.IO;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Snake
 {
     class FileHandler
     {
         private string _fileName;
+        private const byte MAX_HIGHSCORE_ENTRIES = 10;
 
         public FileHandler(string fileName)
         {
@@ -19,62 +22,71 @@ namespace Snake
 
         private void CreateFile()
         {
-            XmlDocument doc = new XmlDocument();
-            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            doc.AppendChild(xmlDeclaration);
-            XmlElement rootNode = doc.CreateElement("Players");
-            doc.AppendChild(rootNode);
-            doc.Save(this._fileName);
+            XDocument xDoc = new XDocument(
+                        new XDeclaration("1.0", "UTF-16", null),
+                        new XElement("Players"));
+
+            xDoc.Save(this._fileName);
         }
 
         public void SaveUserScore(string userName, int score)
         {
-            XmlDocument doc = new XmlDocument();
-            // TODO: Check if the passed score > bottom-most node score and append if so
-            doc.Load(this._fileName);
+            XElement root = XElement.Load(this._fileName);
 
-            XmlNode node = doc.CreateNode(XmlNodeType.Element, "Player", null);
-
-            XmlNode nodeName = doc.CreateElement("Name");
-            nodeName.InnerText = userName;
-
-            XmlNode nodeScore = doc.CreateElement("Score");
-            nodeScore.InnerText = score.ToString();
-
-            XmlNode nodeTime = doc.CreateElement("Time");
-            nodeTime.InnerText = DateTime.Now.ToString();
-
-            node.AppendChild(nodeName);
-            node.AppendChild(nodeScore);
-            node.AppendChild(nodeTime);
-
-            doc.DocumentElement.AppendChild(node);
-
-            // TODO: Linq child nodes sorting by score.
-            doc.Save(this._fileName);
-        }
-
-        // Sample implementation. TODO: Integrate with the high scores menu function.
-        public void PrintScores()
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(this._fileName);
-
-            XmlNodeList nodeList = doc.GetElementsByTagName("Player");
-            foreach (XmlNode node in nodeList)
+            XElement lastPlayerNode = (XElement)root.LastNode;
+            int lastPlayerScore = 0;
+            try
             {
-                XmlElement playerNode = (XmlElement)node;
-
-                string playerName = playerNode.GetElementsByTagName("Name")[0].InnerText;
-                string playerScore = playerNode.GetElementsByTagName("Score")[0].InnerText;
-                string time = playerNode.GetElementsByTagName("Time")[0].InnerText;
-                Console.WriteLine("Username: {0} Score: {1} Date: {2}", playerName, playerScore, time);
+                lastPlayerScore = int.Parse(lastPlayerNode.Element("Score").Value);
+            }
+            catch (NullReferenceException) { }
+            finally
+            {
+                if (score > lastPlayerScore)
+                {
+                    if (root.Elements("Player").Count() == MAX_HIGHSCORE_ENTRIES)
+                    {
+                        root.Elements("Player").Last().Remove();
+                    }
+                    root.Add(new XElement("Player",
+                                            new XElement("Username", userName),
+                                            new XElement("Score", score),
+                                            new XElement("Date", DateTime.Now.ToString())
+                                         )
+                              );
+                    var sortedScores = root.Elements("Player")
+                                          .OrderByDescending(xScore => (int)xScore.Element("Score"))
+                                          .ToArray();
+                    root.RemoveAll();
+                    foreach (XElement sortedScore in sortedScores)
+                    {
+                        root.Add(sortedScore);
+                    }
+                    root.Save(this._fileName);
+                }
             }
         }
 
-        public void GetScoreByUsername(string userName)
+        // TODO: Integrate this with the high scores menu function.
+        public void PrintScores()
         {
-            // TODO
+            XElement root = XElement.Load(this._fileName);
+            IEnumerable<XElement> players = root.Elements();
+            Console.WriteLine("High Scores");
+            foreach (var player in players)
+            {
+                Console.Write("Username: {0} ", player.Element("Username").Value);
+                Console.Write("Score: {0} ", player.Element("Score").Value);
+                Console.Write("Date: {0} ", player.Element("Date").Value);
+                Console.WriteLine();
+            }
         }
+
+        public bool IsUsernamePresent(string userName)
+        {
+            XElement root = XElement.Load(this._fileName);
+            return root.Elements("Player").Where(x => x.Element("Username").Value == userName).Any();
+        }
+
     }
 }
